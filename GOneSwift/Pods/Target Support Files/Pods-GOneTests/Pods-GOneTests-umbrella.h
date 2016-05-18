a46d99b6a3e5177ad6d1d31f2e28a6f955228934
ganjianping <ganjpxm@gmail.com>
2016-05-18 23:55:53 +0800

    GOne

---
+++ GOneSwift/Pods/Target Support Files/Pods-GOneTests/Pods-GOneTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_GOneTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GOneTestsVersionString[];
+
---
+++ GOneSwift/Pods/Target Support Files/Pods-GOneUITests/Pods-GOneUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_GOneUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_GOneUITestsVersionString[];
+
